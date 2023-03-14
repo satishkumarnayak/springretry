ad48a87e6764d93f7fb7acec54ef91e389287073
Satish Nayak <satish_nayak@epam.com>
2023-03-14 15:50:38 -0400

    Springboot start

---
+++ src/test/java/com/instanceofcake/springretry/SpringretryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.springretry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringretryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
